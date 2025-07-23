c202d0f0a8d73f3eea75b09977a23108e7ff1b1f
meowveloper-edu <meowveloper.edu@gmail.com>
2025-07-24 05:14:59 +0700

    ma thi

---
+++ exercises-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int one(void);
+
+int main(void) {
+    one();
+    return 0;
+}
+
+int one(void)
+{
+
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+    return 0;
+}
